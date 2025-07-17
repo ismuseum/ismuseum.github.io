cbc485836488077b98cbde62080b851857a231c1
Opal Lei <61306489+opallei@users.noreply.github.com>
2025-07-16 22:32:21 -0700

    Fixed menu behavior in mobile. Adjusted display under fixed header. (#1)
    Authored-by: opallei

--- main.js
+++ main.js
@@ -1,13 +1,33 @@
 
 $(document).ready(function() {
+  // Expand menu when clicking hamburger icon.
   $('.menu-btn').click(function() {
     $('.menu-list').toggleClass("show");
   });
 
-  // Optional: Close menu when clicking outside
-  $(document).click(function(e) {
-    if (!$(e.target).closest('.menu-btn, .menu-list').length) {
-      $('.menu-list').removeClass('show');
+  // Open external links in new tab.
+  $('a[href^="http"]').attr('target','_blank').attr('rel','noopener noreferrer');
+
+  // Adjust display for fixed header height.
+  $('a[href*="#"]:not([href="#"])').click(function() {
+    $('.menu-list').removeClass('show');
+    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') || location.hostname == this.hostname) {
+      var target = $(this.hash);
+      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
+      if (target.length) {
+        var headerHeight = 0;
+        // Get fixed header height
+        if (window.matchMedia('(max-width: 768px)').matches) {
+          headerHeight = $('#fixed-header').height();  // When the screen width is 768px or less
+        } else {
+          headerHeight = $('#fixed-header').height() + $('.menu-list').height();  // When the screen width is greater
+        }
+        $('html,body').animate({
+          scrollTop: target.offset().top - headerHeight
+        }, 400); // Adjust scroll position by subtracting headerHeight
+        return false;
+      }
     }
   });
+
 });
